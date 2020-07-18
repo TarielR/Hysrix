d3ce399ef49130edc26d843f11c55a0325db8421
Tariel <tar.iel.reg@gmail.com>
2020-07-17 20:20:46 -0400

    Some refactoring

--- src/main/java/tariel/HystrixController.java
+++ src/main/java/tariel/HystrixController.java
@@ -1,10 +1,13 @@
 package tariel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SpringBootApplication
 public class HystrixController {
 
     private final HystrixService service;
@@ -16,5 +19,9 @@ public class HystrixController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public String useService() {
         return service.getAddressFromAWS();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixController.class, args);
     }
 }
--- src/main/java/tariel/HystrixService.java
+++ src/main/java/tariel/HystrixService.java
@@ -2,14 +2,11 @@ package tariel;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@SpringBootApplication
 @EnableCircuitBreaker
 public class HystrixService {
 
@@ -18,15 +15,10 @@ public class HystrixService {
 
     @HystrixCommand(fallbackMethod = "getDefaultAddress")
     public String getAddressFromAWS() {
-        var text = new RestTemplate().getForObject(URL, String.class);
-        return text;
+        return new RestTemplate().getForObject(URL, String.class);
     }
 
     public String  getDefaultAddress() {
         return "Default address";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HystrixService.class, args);
     }
 }
